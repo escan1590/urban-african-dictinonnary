46771db16ae1de839863b97744e39ccdac46eabb
Daniel Paul Tchatchouang Ouokam <tchatchouang.ouokam@gmail.com>
2021-06-02 08:42:56 +0200

    add more migrations

---
+++ Backend/migrations/sqls/20210602062036-seed-table-category-up.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (title, path) VALUES('dance','dance');
+INSERT INTO category (title, path) VALUES('sex','sex');
+INSERT INTO category (title, path) VALUES('music','music');
+INSERT INTO category (title, path) VALUES('ethny','ethny');
+INSERT INTO category (title, path) VALUES('school','school');
+INSERT INTO category (title, path) VALUES('name','name');
+INSERT INTO category (title, path) VALUES('work','work');
+INSERT INTO category (title, path) VALUES('street','street');
