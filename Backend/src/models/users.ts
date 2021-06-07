2fdaf6c9dd43c6c498af1204bbd21377159a95f7
Daniel Paul Tchatchouang Ouokam <tchatchouang.ouokam@gmail.com>
2021-06-07 22:46:54 +0200

    modify users test and add users models

--- Backend/src/models/users.ts
+++ Backend/src/models/users.ts
@@ -1,13 +1,104 @@
-import Client from "../database";
-
+/* eslint-disable import/extensions */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/extensions
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import Client from '../database';
 // Type for users data
+dotenv.config();
+const pepper = process.env.BCRYPT_PASSWORD;
+const saltRounds = process.env.SALT_ROUNDS as string | number;
 export type User = {
   id: string;
   name: string;
   username: string;
   email: string;
   passwordHash: string;
-  registeredAt: Date;
-    }
+  registeredAt: string;
+};
 
-export class UserStore {}
+export class UserStore {
+  async index(): Promise<User[]> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM users';
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Unable to fetch users : ${error}`);
+    }
+  }
+
+  async show(username: string): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM users WHERE username=($1)';
+      const result = await conn.query(sql, [username]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Unable to fetch user : ${error}`);
+    }
+  }
+
+  async create(
+    id: string,
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    registeredAt: string
+  ): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sqlCheck = 'SELECT username FROM users WHERE username=($1)';
+      const resultCheck = await conn.query(sqlCheck, [username]);
+      if (!resultCheck.rows.length) throw new Error('User Already exist');
+      const sql =
+        'INSERT INTO users (id,name, username, email, passwordHash, resgisteredAt) VALUES($1,$2,$3,$4,$5,$6)';
+      const hash = bcrypt.hashSync(password + pepper, Number(saltRounds));
+      const result = await conn.query(sql, [
+        id,
+        name,
+        username,
+        email,
+        hash,
+        registeredAt,
+      ]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Unable to create user : ${error}`);
+    }
+  }
+
+  async delete(username: string): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'DELETE FROM users WHERE usename=($1)';
+      const result = await conn.query(sql, [username]);
+      const user = result.rows[0];
+      conn.release();
+      return user;
+    } catch (error) {
+      throw new Error(`Unable to delete the user : ${error}`);
+    }
+  }
+
+  async authenticate(username: string, password: string): Promise<User | null> {
+    const conn = await Client.connect();
+    const sql = 'SELECT username,password FROM users WHERE username=($1)';
+    const result = await conn.query(sql, [username]);
+    if (result.rows.length) {
+      const user = result.rows[0];
+      if (bcrypt.compareSync(password + pepper, user.passwordHash)) {
+        return user;
+      }
+    }
+    conn.release();
+    return null;
+  }
+}
--- Backend/src/tests/models/usersSpec.ts
+++ Backend/src/tests/models/usersSpec.ts
@@ -1,94 +1,147 @@
-import { UserStore, User } from "../../models/users";
-import {v4 as uuid} from "uuid"
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
+import { v4 as uuid } from 'uuid';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import { UserStore } from '../../models/users';
 
+dotenv.config();
+const pepper = process.env.BCRYPT_PASSWORD as string;
 const store = new UserStore();
 
-//tests for definition model
-describe("users model", () => {
-  //fist part: test if the methods are defined
-  describe("method definition", () => {
-    it("should have an index method", () => {
+// tests for definition model
+describe('users model', () => {
+  // fist part: test if the methods are defined
+  describe('method definition', () => {
+    it('should have an index method', () => {
       expect(store.index).toBeDefined();
     });
-    it("should have a show method", () => {
+    it('should have a show method', () => {
       expect(store.show).toBeDefined();
     });
-    it("should have a create method", () => {
+    it('should have a create method', () => {
       expect(store.create).toBeDefined();
     });
-    it("should have an update method", () => {
-      expect(store.update).toBeDefined();
-    });
-    it("should have a delete method", () => {
+    // it("should have an update method", () => {
+    //   expect(store.update).toBeDefined();
+    // });
+    it('should have a delete method', () => {
       expect(store.delete).toBeDefined();
     });
   });
 
-  //second part: test if the different method behave as intended
-  describe("method behavior",
-    () => {
-        const userId = uuid();
-      it("should create a new user", async () => {
-        const newUser = await store.create(
-          userId,
-          "daniel",
-          "dan",
-          "dan@gmail.com",
-          "1234",
-          "2020-12-12"
-        );
-        expect(newUser).toEqual({
-          id: userId,
-          name: "daniel",
-          username: "dan",
-          email: "dan@gmail.com",
-          passwordHash: "1234",
-          registeredAt: "2020-12-12",
-        });
-      });
-      it("should display all the users created", async () => {
-        const result = await store.index();
-        expect(result).toEqual([{
-          id: userId,
-          name: "daniel",
-          username: "dan",
-          email: "dan@gmail.com",
-          passwordHash: "1234",
-          registeredAt: "2020-12-12",
-        }])
-      });
-      it(`should show the tuple with ${userId} `, () => {
-        const result = await store.show(userId);
-        expect(result).toEqual({
-          id: userId,
-          name: "daniel",
-          username: "dan",
-          email: "dan@gmail.com",
-          passwordHash: "1234",
-          registeredAt: "2020-12-12",
-        })
-      });
-      it("should update the userInfo", async () => {
-        const result = await store.update(userId,"newDaniel","newDan","newMail@gm.co","newPassword");
-        expect(result).toEqual({
-          id: userId,
-          name: "newDaniel",
-          username: "newDan",
-          email: "newMail@gm.co",
-          passwordHash: "newPassword",
-          registeredAt: "2020-12-12",
-        })
-      });
-      it("should delete the tuple with id 1", async () => {
-        const result = await store.delete(userId)
-        expect(result).toEqual({
-          id: userId,
-          name: "daniel",
-          username: "dan",
-          email: "dan@gmail.com",
-          passwordHash: "1234",
-          registeredAt: "2020-12-12",
-        })
-      });
-    };
+  // second part: test if the different method behave as intended
+  describe('method behavior', () => {
+    const userId = uuid();
+    it('should create a new user', async () => {
+      const newUser = await store.create(
+        userId,
+        'daniel',
+        'dan',
+        'dan@gmail.com',
+        '1234',
+        '2020-12-12'
+      );
+      expect(newUser.id).toEqual(userId);
+      expect(newUser.name).toEqual('daniel');
+      expect(newUser.username).toEqual('dan');
+      expect(newUser.email).toEqual('dan@gmail.com');
+      expect(
+        bcrypt.compareSync(`1234${pepper}`, newUser.passwordHash)
+      ).toBeTrue();
+      expect(newUser.registeredAt).toEqual('2020-12-12');
+      // expect(newUser).toEqual({
+      //   id: userId,
+      //   name: "daniel",
+      //   username: "dan",
+      //   email: "dan@gmail.com",
+      //   passwordHash: "1234",
+      //   registeredAt: "2020-12-12",
+      // });
+    });
+
+    it('should display the user auhenticated', async () => {
+      const result = await store.authenticate('dan', '1234');
+      expect(result?.id).toEqual(userId);
+      expect(result?.name).toEqual('daniel');
+      expect(result?.username).toEqual('dan');
+      expect(result?.email).toEqual('dan@gmail.com');
+      expect(
+        bcrypt.compareSync(`1234${pepper}`, result!.passwordHash)
+      ).toBeTrue();
+      expect(result?.registeredAt).toEqual('2020-12-12');
+    });
+
+    it('should display all the users created', async () => {
+      const result = await store.index();
+      expect(result[0].id).toEqual(userId);
+      expect(result[0].name).toEqual('daniel');
+      expect(result[0].username).toEqual('dan');
+      expect(result[0].email).toEqual('dan@gmail.com');
+      expect(
+        bcrypt.compareSync(`1234${pepper}`, result[0].passwordHash)
+      ).toBeTrue();
+      expect(result[0].registeredAt).toEqual('2020-12-12');
+      // expect(result).toEqual([
+      //   {
+      //     id: userId,
+      //     name: 'daniel',
+      //     username: 'dan',
+      //     email: 'dan@gmail.com',
+      //     passwordHash: '1234',
+      //     registeredAt: '2020-12-12',
+      //   },
+      // ]);
+    });
+    it(`should show the tuple with ${userId} `, async () => {
+      const result = await store.show('dan');
+      expect(result.id).toEqual(userId);
+      expect(result.name).toEqual('daniel');
+      expect(result.username).toEqual('dan');
+      expect(result.email).toEqual('dan@gmail.com');
+      expect(
+        bcrypt.compareSync(`1234${pepper}`, result.passwordHash)
+      ).toBeTrue();
+      expect(result.registeredAt).toEqual('2020-12-12');
+      // expect(result).toEqual({
+      //   id: userId,
+      //   name: "daniel",
+      //   username: "dan",
+      //   email: "dan@gmail.com",
+      //   passwordHash: "1234",
+      //   registeredAt: "2020-12-12",
+      // })
+    });
+    // it("should update the userInfo", async () => {
+    //   const result = await store.update(userId,"newDaniel","newDan","newMail@gm.co","newPassword");
+    //   expect(result).toEqual({
+    //     id: userId,
+    //     name: "newDaniel",
+    //     username: "newDan",
+    //     email: "newMail@gm.co",
+    //     passwordHash: "newPassword",
+    //     registeredAt: "2020-12-12",
+    //   })
+    // });
+    it('should delete the tuple with id 1', async () => {
+      const result = await store.delete(userId);
+      expect(result.id).toEqual(userId);
+      expect(result.name).toEqual('daniel');
+      expect(result.username).toEqual('dan');
+      expect(result.email).toEqual('dan@gmail.com');
+      expect(
+        bcrypt.compareSync(`1234${pepper}`, result.passwordHash)
+      ).toBeTrue();
+      expect(result.registeredAt).toEqual('2020-12-12');
+      // expect(result).toEqual({
+      //   id: userId,
+      //   name: "daniel",
+      //   username: "dan",
+      //   email: "dan@gmail.com",
+      //   passwordHash: "1234",
+      //   registeredAt: "2020-12-12",
+      // })
+    });
+  });
 });
